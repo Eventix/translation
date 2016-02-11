3d04436461804ce02ac5d3045097f2352ab16add
Mart Pluijmaekers <mart@tuupke.nl>
2016-02-11 13:40:11 +0100

    Renaming migration to the proper name

--- database/2016_02_11_114323_eventix_translation_create_database_table.php
+++ database/2016_02_11_114323_eventix_translation_create_database_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EventixTranslationDatabaseMigration extends Migration
+class EventixTranslationCreateDatabaseTable extends Migration
 {
     /**
      * Run the migrations.
